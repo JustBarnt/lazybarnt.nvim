923ab8bb0bca55523ae849a2f5335b4b3a2bd79e
bwilliams <bwilliams@commsys.com>
2024-07-29 16:57:03 -0400

    chore(reverted noice cmdline settings)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,49 +16,6 @@ return {
           silent = true,
         },
       },
-      views = {
-        cmdline_popup = {
-          position = {
-            row = 5,
-            col = "50%",
-          },
-          border = {
-            style = "none",
-            padding = { 1, 1 },
-          },
-          size = {
-            width = 60,
-            height = "auto",
-          },
-          win_options = {
-            winhighlight = {
-              Normal = "TelescopePromptNormal",
-              FloatBorder = "TelescopePromptNormal",
-            },
-          },
-        },
-        popupmenu = {
-          relative = "editor",
-          position = {
-            row = 8,
-            col = "50%",
-          },
-          size = {
-            width = 60,
-            height = 10,
-          },
-          border = {
-            style = "none",
-            padding = { 1, 1 },
-          },
-          win_options = {
-            winhighlight = {
-              Normal = "TelescopeNormal",
-              FloatBorder = "TelescopeNormal",
-            },
-          },
-        },
-      },
     },
   },
   {
