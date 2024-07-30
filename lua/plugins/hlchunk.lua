b32987a8b7c424d0edfc5fe418ad467610499c3c
bwilliams <bwilliams@commsys.com>
2024-07-30 12:59:00 -0400

    feat: disable indent-blankline in favor of HLChunk

---
+++ lua/plugins/hlchunk.lua
@@ -0,0 +1,34 @@
+return {
+  { "indent-blankline.nvim", enabled = false },
+  {
+    "shellRaining/hlchunk.nvim",
+    event = { "CursorHold", "CursorHoldI" },
+    opts = {
+      chunk = {
+        enable = true,
+        chars = {
+          horizontal_line = "─",
+          vertical_line = "│",
+          left_top = "╭",
+          left_bottom = "╰",
+          right_arrow = "─",
+        },
+      },
+      indent = {
+        enable = true,
+        chars = {
+          "│",
+        },
+        style = {
+          vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID("Whitespace")), "fg", "gui"),
+        },
+      },
+      line_num = {
+        enable = false,
+      },
+      blank = {
+        enable = false,
+      },
+    },
+  },
+}
