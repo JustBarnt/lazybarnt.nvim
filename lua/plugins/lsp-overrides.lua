8a723948ce7e274aadd6b925f67e1999f2044c4c
bwilliams <bwilliams@commsys.com>
2024-08-01 10:11:33 -0400

    update(lsp): make tailwindcss only start if it detects specified files

--- lua/plugins/lsp-overrides.lua
+++ lua/plugins/lsp-overrides.lua
@@ -19,6 +19,20 @@ return {
             },
           },
         },
+        tailwindcss = {
+          root_dir = require("lspconfig.util").root_pattern(
+            "tailwind.config.js",
+            "tailwind.config.cjs",
+            "tailwind.config.mjs",
+            "tailwind.config.ts",
+            "postcss.config.js",
+            "postcss.config.cjs",
+            "postcss.config.mjs",
+            "postcss.config.ts",
+            "package.json",
+            "node_modules"
+          ),
+        },
       },
     },
   },
