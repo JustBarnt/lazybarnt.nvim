9337d4be9cf6a889d0523dc254d79091f381324c
bwilliams <bwilliams@commsys.com>
2024-08-16 08:58:44 -0400

    chore: typo

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.bigfile_size = 1024 * 1024 * 5
-vim.g.vim.g.lazyvim_picker = "telescope"
+vim.g.lazyvim_picker = "telescope"
 vim.g.lazyvim_php_lsp = "intelephense"
 vim.g.lazyvim_statuscolumn = {
   folds_open = true,
