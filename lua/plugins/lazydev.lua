5247cb40f4cdd804d189d48a298aea71c74b5c31
justbarnt <functnal@gmail.com>
2024-08-20 08:19:57 -0400

    added luarocks.nvim

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -5,6 +5,7 @@ return {
     opts = function(_, opts)
       vim.list_extend(opts.library, {
         { path = "wezterm-types", mods = { "wezterm" } },
+        { path = "luafilesystem", words = { "lfs%." } },
       })
     end,
   },
---
+++ lua/plugins/tooling.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 10000,
+    config = true,
+  },
+}
