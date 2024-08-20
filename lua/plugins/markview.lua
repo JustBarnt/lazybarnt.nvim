a12b7dfadbbf961887b86e916740359ce002e3ad
bwilliams <bwilliams@commsys.com>
2024-08-20 11:35:09 -0400

    updates to markview

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -26,13 +26,37 @@ return {
             value = { fg = "#a6e3a1" },
           },
         },
+        links = {
+          inline_links = {
+            hl = "@markup.link.label.markown_inline",
+            icon = " ",
+            icon_hl = "@markup.link",
+          },
+          images = {
+            hl = "@markup.link.label.markown_inline",
+            icon = " ",
+            icon_hl = "@markup.link",
+          },
+        },
         code_blocks = {
           style = "minimal",
           icons = true,
         },
+        list_items = {
+          shift_width = 2,
+          marker_minus = { text = "●", hl = "@markup.list.markdown" },
+          marker_plus = { text = "●", hl = "@markup.list.markdown" },
+          marker_star = { text = "●", hl = "@markup.list.markdown" },
+          marker_dot = {},
+        },
+        inline_codes = { enable = false },
         headings = {
-          enable = true,
-          shift_width = 0,
+          heading_1 = { style = "simple", hl = "Headline1" },
+          heading_2 = { style = "simple", hl = "Headline2" },
+          heading_3 = { style = "simple", hl = "Headline3" },
+          heading_4 = { style = "simple", hl = "Headline4" },
+          heading_5 = { style = "simple", hl = "Headline5" },
+          heading_6 = { style = "simple", hl = "Headline6" },
         },
         horizontal_rules = {
           parts = {
