d7c754cbed178921b3496dafe054d44bb331e113
Brent Williams <functnal@gmail.com>
2024-09-09 15:59:54 -0400

    chore

--- init.lua
+++ init.lua
@@ -1,13 +1,3 @@
-if vim.fn.has("win32") == 1 then
-  local ori_fnameescape = vim.fn.fnameescape
-  ---@diagnostic disable-next-line: duplicate-set-field
-  vim.fn.fnameescape = function(...)
-    ---@type string
-    local result = ori_fnameescape(...)
-    return result:gsub("\\", "/")
-  end
-end
-
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
