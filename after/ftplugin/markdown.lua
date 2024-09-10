339e76ff6fff5ebf6c8e41b5e007d4f53ac62018
Brent Williams <functnal@gmail.com>
2024-09-10 11:05:09 -0400

    update: fixed custom md todo completion keymaps

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,13 +1,13 @@
 vim.keymap.set("n", "<leader>mc", function()
   vim.cmd([[
-  s#^- \[ \]#- [x]
+  s#- \[ \]#- [x]
   ]])
   vim.cmd([[nohlsearch]])
 end, { desc = "Mark TODO Completed", silent = true })
 
 vim.keymap.set("n", "<leader>mC", function()
   vim.cmd([[
-  s#^- \[x\]#- [ ]
+  s#- \[x\]#- [ ]
   ]])
   vim.cmd([[nohlsearch]])
 end, { desc = "Mark TODO Not Completed", silent = true })
