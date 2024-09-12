45be962f4042044992c2b45fd50b624bee778e7d
Brent Williams <functnal@gmail.com>
2024-09-12 13:14:25 -0400

    restore cursor position

--- lua/utils/strings.lua
+++ lua/utils/strings.lua
@@ -8,12 +8,14 @@ M.escape_pattern = function(pattern)
 end
 
 M.replace_word_instance_under_cursor = function()
+  local cpos = vim.api.nvim_win_get_cursor(vim.api.nvim_get_current_win())
   local word = vim.fn.expand("<cword>")
   local prompt = "Replace: %s Length:[%d]:"
   vim.ui.input({ prompt = string.format(prompt, word, #word) }, function(input)
     local cmd = "%%s#%s#%s#g"
     if input and #input > 0 then
       vim.cmd(string.format(cmd, word, input))
+      vim.api.nvim_win_set_cursor(vim.api.nvim_get_current_win(), cpos)
     end
   end)
 end
