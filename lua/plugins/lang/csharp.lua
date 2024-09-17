4699215c0bc8071336192b7b09e07fcf0fe376ee
Brent Williams <functnal@gmail.com>
2024-09-17 12:04:49 -0400

    feat(Roslyn): Added support to work with Rosyln.nvim

--- lua/plugins/lang/csharp.lua
+++ lua/plugins/lang/csharp.lua
@@ -14,13 +14,13 @@ return {
         config = {
           capabilities = vim.tbl_deep_extend(
             "force",
-            {},
+            require("lang.lsp.csharp_capabilities"),
             vim.lsp.protocol.make_client_capabilities(),
             has_cmp and cmp_nvim_lsp.default_capabilities() or {},
             opts.capabiltites or {}
           ),
         },
-        filewatching = false,
+        filewatching = true,
       }
       return config
     end,
