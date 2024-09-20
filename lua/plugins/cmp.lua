5606f12e867dc5d8642b8a470884bc2c841e0087
justbarnt <functnal@gmail.com>
2024-09-20 09:15:19 -0400

    fix cmp so cmdline cmp runs from start

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -20,6 +20,62 @@ return {
         return require("cmp.types").lsp.CompletionItemKind[entry:get_kind()] ~= "Text"
       end
 
+      return {
+        auto_brackets = {},
+        completion = {
+          completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
+        },
+        experimental = {
+          ghost_text = {
+            enabled = false,
+            hl_group = "CmpGhostText",
+          },
+        },
+        formatting = {
+          format = function(entry, item)
+            local icons = LazyVim.config.icons.kinds
+            if icons[item.kind] then
+              item.kind = icons[item.kind] .. item.kind
+            end
+
+            local widths = {
+              abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
+              menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
+            }
+
+            for key, width in pairs(widths) do
+              if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+                item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
+              end
+            end
+
+            return item
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-j>"] = cmp.mapping.select_next_item(),
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
+          ["<S-j>"] = cmp.mapping.scroll_docs(-4),
+          ["<S-k>"] = cmp.mapping.scroll_docs(4),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<C-y>"] = cmp.mapping(cmp.mapping.confirm({ select = true }), { "i", "c" }),
+          ["<C-Space>"] = cmp.mapping.complete({}),
+        }),
+        preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "lazydev", group_index = 0 },
+          { name = "cmdline" },
+          { name = "path", option = { keyword_pattern = "[[/|\\]]" } },
+        }, {
+          { name = "buffer", option = { keyword_lenth = 4 } },
+        }),
+        sorting = defaults.sorting,
+      }
+    end,
+    config = function(_, opts)
+      local cmp = require("cmp")
+      cmp.setup(opts)
       cmp.setup.cmdline({ "/", "?" }, {
         sources = cmp.config.sources({
           { name = "buffer" },
@@ -85,61 +141,7 @@ return {
           },
         },
       })
-
-      return {
-        auto_brackets = {},
-        completion = {
-          completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
-        },
-        experimental = {
-          ghost_text = {
-            enabled = false,
-            hl_group = "CmpGhostText",
-          },
-        },
-        formatting = {
-          format = function(entry, item)
-            local icons = LazyVim.config.icons.kinds
-            if icons[item.kind] then
-              item.kind = icons[item.kind] .. item.kind
-            end
-
-            local widths = {
-              abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
-              menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
-            }
-
-            for key, width in pairs(widths) do
-              if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
-                item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
-              end
-            end
-
-            return item
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-j>"] = cmp.mapping.select_next_item(),
-          ["<C-k>"] = cmp.mapping.select_prev_item(),
-          ["<S-j>"] = cmp.mapping.scroll_docs(-4),
-          ["<S-k>"] = cmp.mapping.scroll_docs(4),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<C-y>"] = cmp.mapping(cmp.mapping.confirm({ select = true }), { "i", "c" }),
-          ["<C-Space>"] = cmp.mapping.complete({}),
-        }),
-        preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "lazydev", group_index = 0 },
-          { name = "cmdline" },
-          { name = "path", option = { keyword_pattern = "[[/|\\]]" } },
-        }, {
-          { name = "buffer", option = { keyword_lenth = 4 } },
-        }),
-        sorting = defaults.sorting,
-      }
     end,
-    main = "lazyvim.util.cmp",
   },
   -- Snippets
   {
