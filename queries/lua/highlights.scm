6c4e0badd27dc95db3c800a5144b2cba489d97b1
justbarnt <functnal@gmail.com>
2024-09-26 07:56:42 -0400

    new query

---
+++ queries/lua/highlights.scm
@@ -0,0 +1,14 @@
+;; extends
+
+((identifier) @namespace.builtin
+  (#eq? @namespace.builtin "vim"))
+
+((identifier) @namespace.builtin
+  (#eq? @namespace.builtin "NvVim"))
+
+; Operators
+[
+  "and"
+  "not"
+  "or"
+] @keyword.debug
