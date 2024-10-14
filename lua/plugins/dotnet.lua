4f14f2fd807190c969134c9b6e60a312805ed5db
bwilliams <bwilliams@commsys.com>
2024-10-14 12:27:25 -0400

    dotnet plugin

---
+++ lua/plugins/dotnet.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "MoaidHathot/dotnet.nvim",
+    cmd = "DotnetUI",
+    opts = {
+      bootstrap = {
+        auto_bootstrap = true,
+      },
+      project_selection = {
+        path_display = "filename_first",
+      },
+    },
+  },
+}
