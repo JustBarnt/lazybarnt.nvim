7eca1ff748a8af8fd09b104429cbfadfbd707a50
justbarnt <functnal@gmail.com>
2024-12-02 14:23:55 -0500

    chore: update blink and fix latest breaking changes

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -2,12 +2,10 @@ return {
   { "saghen/blink.compat" },
   {
     "saghen/blink.cmp",
-    enabled = true,
     version = "*",
     ---@module "blink.cmp"
     ---@type blink.cmp.Config
     opts = {
-      highlight = { use_nvim_cmp_as_default = true },
       keymap = {
         preset = "default",
         ["<C-j>"] = { "select_next", "accept" },
@@ -15,42 +13,131 @@ return {
         ["Up"] = { "scroll_documentation_up" },
         ["Down"] = { "scroll_documentation_down" },
       },
-      nerd_font_variant = "mono",
-      windows = {
-        autocomplete = {
-          border = "rounded",
-          highlight = "Normal:Pmenu,CursorLine:PmenuSel,Search:None,FloatBorder:Pmenu",
+      completion = {
+        list = {
+          max_items = 100,
           selection = "preselect",
+          cycle = {
+            from_bottom = true,
+            from_top = true,
+          },
+        },
+        accept = {
+          create_undo_point = true,
+          auto_brackets = {
+            enabled = true,
+            default_brackets = { "(", ")" },
+            override_brackets_for_filetypes = {},
+            kind_resolution = {
+              enabled = true,
+              blocked_filetypes = { "typescriptreact", "javascriptreact", "vue" },
+            },
+            semantic_token_resolution = {
+              enabled = true,
+              blocked_filetypes = {},
+              timeout_ms = 400,
+            },
+          },
+        },
+
+        menu = {
+          enabled = true,
+          min_width = 15,
+          max_height = 10,
+          border = "rounded",
+          winblend = 0,
+          winhighlight = "Normal:BlinkCmpMenu,FloatBorder:BlinkCmpMenuBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
+          scrolloff = 2,
+          scrollbar = true,
+          direction_priority = { "s", "n" },
           draw = {
             columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } },
           },
         },
-        documentation = {
+      },
+
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 500,
+        update_delay_ms = 50,
+        treesitter_highlighting = true,
+        window = {
+          min_width = 10,
+          max_width = 60,
+          max_height = 20,
           border = "rounded",
-          winhighlight = "Normal:Pmenu,FloatBorder:Pmenu",
-          auto_show = true,
-          auto_show_delay_ms = 250,
-        },
-        signature_help = {
-          border = "padded",
-          winhighlight = "Normal:NormalFloat,CursorLine:PmenuSel,Search:None,FloatBorder:Pmenu",
-        },
-      },
-      sources = {
-        completion = {
-          enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev" },
-        },
-        providers = {
-          lsp = {
-            -- Don't let luaLS show require statements when lazydev has items
-            fallback_for = { "lazydev" },
-          },
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
+          winblend = 0,
+          winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+          scrollbar = true,
+          direction_priority = {
+            menu_north = { "e", "w", "n", "s" },
+            menu_south = { "e", "w", "s", "n" },
           },
         },
       },
+      ghost_text = {
+        enabled = true,
+      },
+    },
+  },
+
+  sources = {
+    completion = {
+      enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev" },
+    },
+
+    providers = {
+      lsp = {
+        -- Don't let luaLS show require statements when lazydev has items
+        fallback_for = { "lazydev" },
+      },
+      lazydev = {
+        name = "LazyDev",
+        module = "lazydev.integrations.blink",
+      },
+    },
+  },
+
+  appearance = {
+    highlight_ns = vim.api.nvim_create_namespace("blink_cmp"),
+    -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+    -- Useful for when your theme doesn't support blink.cmp
+    -- Will be removed in a future release
+    use_nvim_cmp_as_default = true,
+    -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+    -- Adjusts spacing to ensure icons are aligned
+    nerd_font_variant = "mono",
+    kind_icons = {
+      Text = "󰉿",
+      Method = "󰊕",
+      Function = "󰊕",
+      Constructor = "󰒓",
+
+      Field = "󰜢",
+      Variable = "󰆦",
+      Property = "󰖷",
+
+      Class = "󱡠",
+      Interface = "󱡠",
+      Struct = "󱡠",
+      Module = "󰅩",
+
+      Unit = "󰪚",
+      Value = "󰦨",
+      Enum = "󰦨",
+      EnumMember = "󰦨",
+
+      Keyword = "󰻾",
+      Constant = "󰏿",
+
+      Snippet = "󱄽",
+      Color = "󰏘",
+      File = "󰈔",
+      Reference = "󰬲",
+      Folder = "󰉋",
+      Event = "󱐋",
+      Operator = "󰪚",
+      TypeParameter = "󰬛",
     },
   },
 }
