95172b045e55fd3fbada0e5c2b342cd85c7538a9
justbarnt <functnal@gmail.com>
2025-01-02 10:16:43 -0500

    updated blink.cmp to latest stable v0.9.0

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -14,9 +14,12 @@ return {
         ["Down"] = { "scroll_documentation_down" },
       },
       completion = {
+        ghost_text = { enabled = true },
         list = {
           max_items = 100,
-          selection = "preselect",
+          selection = function(ctx)
+            return ctx.mode == "cmdline" and "auto_insert" or "preselect"
+          end,
           cycle = {
             from_bottom = true,
             from_top = true,
@@ -54,90 +57,98 @@ return {
             columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 1 } },
           },
         },
-      },
-
-      documentation = {
-        auto_show = true,
-        auto_show_delay_ms = 500,
-        update_delay_ms = 50,
-        treesitter_highlighting = true,
-        window = {
-          min_width = 10,
-          max_width = 60,
-          max_height = 20,
-          border = "rounded",
-          winblend = 0,
-          winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
-          scrollbar = true,
-          direction_priority = {
-            menu_north = { "e", "w", "n", "s" },
-            menu_south = { "e", "w", "s", "n" },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500,
+          update_delay_ms = 50,
+          treesitter_highlighting = true,
+          window = {
+            min_width = 10,
+            max_width = 60,
+            max_height = 20,
+            border = "rounded",
+            winblend = 0,
+            winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+            scrollbar = true,
+            direction_priority = {
+              menu_north = { "e", "w", "n", "s" },
+              menu_south = { "e", "w", "s", "n" },
+            },
           },
         },
       },
-      ghost_text = {
-        enabled = true,
+      sources = {
+        default = function(ctx)
+          local success, node = pcall(vim.treesitter.get_node)
+          if vim.bo.filetype == "lua" then
+            return { "lsp", "path" }
+          elseif success and node and vim.tbl_contains({ "comment", "line_comment", "block_comment" }, node:type()) then
+            return { "buffer" }
+          else
+            return { "lsp", "path", "snippets", "buffer", "lazydev" }
+          end
+        end,
+        cmdline = function()
+          local type = vim.fn.getcmdtype()
+          if type == "/" or type == "?" then
+            return { "buffer" }
+          end
+          if type == ":" or type == "@" then
+            return { "cmdline" }
+          end
+          return {}
+        end,
+        providers = {
+          lazydev = {
+            name = "LazyDev",
+            module = "lazydev.integrations.blink",
+            score_offset = 100,
+          },
+        },
       },
-    },
-  },
 
-  sources = {
-    completion = {
-      enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev" },
-    },
+      appearance = {
+        highlight_ns = vim.api.nvim_create_namespace("blink_cmp"),
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- Will be removed in a future release
+        use_nvim_cmp_as_default = false,
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = "mono",
+        kind_icons = {
+          Text = "󰉿",
+          Method = "󰊕",
+          Function = "󰊕",
+          Constructor = "󰒓",
 
-    providers = {
-      lsp = {
-        -- Don't let luaLS show require statements when lazydev has items
-        fallback_for = { "lazydev" },
+          Field = "󰜢",
+          Variable = "󰆦",
+          Property = "󰖷",
+
+          Class = "󱡠",
+          Interface = "󱡠",
+          Struct = "󱡠",
+          Module = "󰅩",
+
+          Unit = "󰪚",
+          Value = "󰦨",
+          Enum = "󰦨",
+          EnumMember = "󰦨",
+
+          Keyword = "󰻾",
+          Constant = "󰏿",
+
+          Snippet = "󱄽",
+          Color = "󰏘",
+          File = "󰈔",
+          Reference = "󰬲",
+          Folder = "󰉋",
+          Event = "󱐋",
+          Operator = "󰪚",
+          TypeParameter = "󰬛",
+        },
       },
-      lazydev = {
-        name = "LazyDev",
-        module = "lazydev.integrations.blink",
-      },
-    },
-  },
-
-  appearance = {
-    highlight_ns = vim.api.nvim_create_namespace("blink_cmp"),
-    -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-    -- Useful for when your theme doesn't support blink.cmp
-    -- Will be removed in a future release
-    use_nvim_cmp_as_default = true,
-    -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-    -- Adjusts spacing to ensure icons are aligned
-    nerd_font_variant = "mono",
-    kind_icons = {
-      Text = "󰉿",
-      Method = "󰊕",
-      Function = "󰊕",
-      Constructor = "󰒓",
-
-      Field = "󰜢",
-      Variable = "󰆦",
-      Property = "󰖷",
-
-      Class = "󱡠",
-      Interface = "󱡠",
-      Struct = "󱡠",
-      Module = "󰅩",
-
-      Unit = "󰪚",
-      Value = "󰦨",
-      Enum = "󰦨",
-      EnumMember = "󰦨",
-
-      Keyword = "󰻾",
-      Constant = "󰏿",
-
-      Snippet = "󱄽",
-      Color = "󰏘",
-      File = "󰈔",
-      Reference = "󰬲",
-      Folder = "󰉋",
-      Event = "󱐋",
-      Operator = "󰪚",
-      TypeParameter = "󰬛",
     },
   },
 }
