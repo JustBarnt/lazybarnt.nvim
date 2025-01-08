a8ed8f2350f0f4cad123dd8e811b2807aed000c6
justbarnt <functnal@gmail.com>
2025-01-08 14:06:33 -0500

    updated roslyn ls

--- lua/plugins/lang/csharp.lua
+++ lua/plugins/lang/csharp.lua
@@ -9,14 +9,14 @@ return {
     "seblj/roslyn.nvim",
     ft = "cs",
     opts = function(_, opts)
-      local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+      local blink = require("blink.cmp")
       local config = {
         config = {
           capabilities = vim.tbl_deep_extend(
             "force",
             require("lang.lsp.csharp_capabilities"),
             vim.lsp.protocol.make_client_capabilities(),
-            {},
+            blink.get_lsp_capabilities(),
             opts.capabiltites or {}
           ),
         },
